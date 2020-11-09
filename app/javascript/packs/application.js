ce3d3522a28c744b7230a7c8754e20c514e61b0c
Max Lestage <maxlestage@icloud.com>
2020-11-09 11:00:48 +0100

    test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,12 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-// Rajouter suivant le tuto
-require ("jquery") 
-require ("jquery_ujs") 
-require ("turbolinks") 
-require ("bootstrap") 
-require ("_tree .")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
