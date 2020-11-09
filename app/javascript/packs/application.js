4a25bfb70e3f7237a58cab0370907d343e9193b8
Max Lestage <maxlestage@icloud.com>
2020-11-09 10:56:21 +0100

    App ready for first deployment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,11 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require ("jquery").start() 
+require ("jquery_ujs").start() 
+require ("turbolinks").start() 
+require ("bootstrap").start() 
+require ("_tree .").start()
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
